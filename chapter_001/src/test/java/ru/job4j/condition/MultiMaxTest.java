702b8fa41239f7ebb3f049355fdd5fba342bbe82
daniil_mysnik_990 <69dantes6901>
2020-07-16 20:30:03 +0600

    [#240796]

---
+++ chapter_001/src/test/java/ru/job4j/condition/MultiMaxTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class MultiMaxTest {
+
+    @Test
+    public void whenFirstMax() {
+        int result = MultiMax.max(10, 4, 2);
+        assertThat(result, is(10));
+    }
+
+    @Test
+    public void whenSecondMax() {
+        int result = MultiMax.max(1, 4, 2);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenThirdMax() {
+        int result = MultiMax.max(1, 4, 15);
+        assertThat(result, is(15));
+    }
+
+}
