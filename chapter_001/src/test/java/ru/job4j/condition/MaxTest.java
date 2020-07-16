4279d2aee3a95bff42feddef2ccc7544f0be9d3a
daniil_mysnik_990 <69dantes6901>
2020-07-16 20:21:10 +0600

    [#240795]

---
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class MaxTest {
+
+    @Test
+    public void whenMax1To2Then2() {
+        int result = Max.max(1, 2);
+        assertThat(result, is(2));
+    }
+
+    @Test
+    public void testWhen2Max() {
+        int result = Max.max(1, 10);
+        assertThat(result, is(10));
+    }
+
+    @Test
+    public void testWhenMatch() {
+        int result = Max.max(5, 5);
+        assertThat(result, is(5));
+    }
+
+}
