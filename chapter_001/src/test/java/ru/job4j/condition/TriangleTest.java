90b5bbea73a9eff98e9c6869f6e786c4a91d0668
daniil_mysnik_990 <69dantes6901>
2020-07-24 20:28:09 +0600

    [#240968]

--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -2,6 +2,7 @@ package ru.job4j.condition;
 
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
@@ -9,14 +10,26 @@ public class TriangleTest {
 
     @Test
     public void whenExist() {
-        boolean result = Triangle.exist(2.0, 2.0, 2.0);
+        Triangle triangle = new Triangle();
+        boolean result = triangle.exist(2.0, 2.0, 2.0);
         assertThat(result, is(true));
     }
 
     @Test
     public void testWhenNotExist() {
-        boolean result = Triangle.exist(1, 1, 10);
+        Triangle triangle = new Triangle();
+        boolean result = triangle.exist(1, 1, 10);
         assertThat(result, is(false));
+    }
+
+    @Test
+    public void period() {
+        Point a = new Point(0, 0);
+        Point b = new Point(4, 0);
+        Point c = new Point(0, 4);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        assertThat(rsl, closeTo(8, 0.001));
     }
 
 }
