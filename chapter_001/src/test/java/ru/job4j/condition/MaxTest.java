2793195ff0d77e411cbb5369c11f298c021bbbb1
daniil_mysnik_990 <69dantes6901>
2020-07-24 20:35:39 +0600

    [#240970]

--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -25,4 +25,28 @@ public class MaxTest {
         assertThat(result, is(5));
     }
 
+    @Test
+    public void testMax3ArgWhen3Max() {
+        int result = Max.max(1, 2, 3);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void testMax3ArgWhen1Max() {
+        int result = Max.max(10, 2, 3);
+        assertThat(result, is(10));
+    }
+
+    @Test
+    public void testMax4ArgWhen3Max() {
+        int result = Max.max(10, 2, 30, 1);
+        assertThat(result, is(30));
+    }
+
+    @Test
+    public void testMax4ArgWhen2Max() {
+        int result = Max.max(10, 20, 3, 5);
+        assertThat(result, is(20));
+    }
+
 }
