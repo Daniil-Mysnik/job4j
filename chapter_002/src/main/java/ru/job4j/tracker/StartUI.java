e314a5befa2176e1c794887c3c8f0a8024ba3b34
daniil_mysnik_990 <69dantes6901>
2020-07-28 01:26:44 +0600

    [#240995]

--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class StartUI {
     public static void main(String[] args) {
         Tracker tracker = new Tracker();
+        System.out.println(Arrays.toString(tracker.findAll()));
         tracker.add(new Item("Item1"));
         System.out.println(tracker.findById(1));
         System.out.println(Arrays.toString(tracker.findAll()));
