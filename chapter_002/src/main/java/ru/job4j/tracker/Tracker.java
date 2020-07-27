ad58a444f8a7efffda8ef527a389a86bbdd82f47
daniil_mysnik_990 <69dantes6901>
2020-07-28 03:27:21 +0600

    [#240994]

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,28 +14,12 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
     }
 
     public Item[] findAll() {
-        Item[] rsl = new Item[size];
-        int index = 0;
-        for (int i = 0; i < size; i++) {
-            if (items[i] != null) {
-                rsl[index] = items[i];
-                index++;
-            }
-        }
-        rsl = Arrays.copyOf(rsl, index);
-        return rsl;
+        return Arrays.copyOf(items, size);
     }
 
     public Item[] findByName(String key) {
@@ -49,6 +33,36 @@ public class Tracker {
         }
         rsl = Arrays.copyOf(rsl, index);
         return rsl;
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public boolean replace(int id, Item item) {
+        boolean rsl = false;
+        int index = indexOf(id);
+        if (index != -1) {
+            item.setId(id);
+            items[index] = item;
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        items[index] = null;
+        size--;
+        System.arraycopy(items, index + 1, items, index, size - index);
+        return true;
     }
 
 }
