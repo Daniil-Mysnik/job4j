7931ee798770de8463658f91d69f5abec0907109
Danon <Mysnik_999@mail.ru>
2020-08-30 18:59:05 +0600

    [#240757]

---
+++ chapter_004/src/main/java/ru/job4j/lambda/PhoneDictionary.java
@@ -0,0 +1,27 @@
+package ru.job4j.lambda;
+
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    public ArrayList<Person> find(String key) {
+        Predicate<Person> byName = person -> person.getName().equals(key);
+        Predicate<Person> bySurname = person -> person.getSurname().equals(key);
+        Predicate<Person> byPhone = person -> person.getPhone().equals(key);
+        Predicate<Person> byAddress = person -> person.getAddress().equals(key);
+        Predicate<Person> combine = byName.or(bySurname).or(byPhone).or(byAddress);
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person person : persons) {
+            if (combine.test(person)) {
+                result.add(person);
+            }
+        }
+        return result;
+    }
+}
