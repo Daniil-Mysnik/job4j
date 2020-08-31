2aead6b48896adb0303fde76569b76363149fda4
daniil_mysnik_990 <69dantes6901>
2020-09-01 01:00:56 +0600

    [#240767]

--- chapter_004/src/main/java/ru/job4j/stream/CutClone.java
+++ chapter_004/src/main/java/ru/job4j/stream/CutClone.java
@@ -2,15 +2,11 @@ package ru.job4j.stream;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
-public class Profiles {
-    List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(Profile::getAddress).distinct().collect(Collectors.toList());
-    }
-
+public class CutClone {
     public static void main(String[] args) {
         List<Profile> profiles = Arrays.asList(new Profile(new Address("Omsk", "Gagarina", 1, 1)),
+                new Profile(new Address("Omsk", "Gagarina", 1, 1)),
                 new Profile(new Address("Novosibirsk", "Green", 10, 1)));
         new Profiles().collect(profiles).forEach(System.out::println);
     }
