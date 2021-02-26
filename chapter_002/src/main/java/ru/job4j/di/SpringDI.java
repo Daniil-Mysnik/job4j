7d545fabc72c90e29ccff6fe15d5f03906f86a2a
daniil_mysnik_990 <69dantes6901>
2021-02-26 15:47:57 +0600

    [#317809]

--- chapter_002/src/main/java/ru/job4j/di/SpringDI.java
+++ chapter_002/src/main/java/ru/job4j/di/SpringDI.java
@@ -8,8 +8,12 @@ public class SpringDI {
         context.scan("ru.job4j");
         context.refresh();
         StartUI ui = context.getBean(StartUI.class);
+        StartUI ui2 = context.getBean(StartUI.class);
         ui.add("Petr Arsentev");
         ui.add("Ivan ivanov");
+        ui2.add("DFIfs");
+        ui2.add("LKUFGHliafas");
         ui.print();
+        ui2.print();
     }
 }
